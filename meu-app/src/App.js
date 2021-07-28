9c166770ab455a4e0924736e76c90d9f749bf466
lucasmetron <lucasmetron@gmail.com>
2021-07-28 16:09:24 -0300

    limitador de digitação

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <TextCounter />
+        <TextCounter limit={20} />
       </div>
     );
   }
--- meu-app/src/TextCounter.js
+++ meu-app/src/TextCounter.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
 
+class TextCounter extends Component { //aqui dentro é criado as variaveis
 
-
-class TextCounter extends Component {
+    static defaultProps = { // serve para setar valores inicias para variaveis
+        limit: 15
+    }
 
     constructor(props) {
         super(props);
+        this.limit = props.limit
         this.state = {
-            totalChars: 0,
+            totalChars: 0, //variavel
+            text: '',
         }
+
+        this.handleChange = this.handleChange.bind(this) //bind é para passar o componente (this) pra dentro da function
+    }
+
+    handleChange(event) {
+        const element = event.target,
+            text = element.value
+
+        if (text.length <= this.props.limit) {
+            this.setState({
+                totalChars: text.length,
+                text: text,
+            })
+        }
+
     }
 
     render() {
-        const state = this.state
-        console.log(this)
+        const { state, props } = this // apenas para nao ficar ditando this.state toda hora
 
         return (
             <div>
                 <h1>Meu Contador</h1>
-                <textarea name="" id="" cols="30" rows="10" />
+                <textarea onChange={this.handleChange} name="" id="" cols="30" rows="10" value={state.text} />
                 <div>
-                    <strong>Total:</strong> {state.totalChars}
+                    <strong>Total:</strong> {state.totalChars} / {props.limit}
                 </div>
             </div>
         );
