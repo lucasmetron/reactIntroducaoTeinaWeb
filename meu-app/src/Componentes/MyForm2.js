7de32f9e55099aa115d145a9f5b5cd2bc6796db2
lucasmetron <lucasmetron@gmail.com>
2021-07-30 15:42:59 -0300

    react create ref

---
+++ meu-app/src/Componentes/MyForm2.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class MyForm2 extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.inputName = React.createRef()
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event) {
+        console.log(this.inputName.current.value)
+        event.preventDefault(); //evitar o padrão do botao submit de enviar formulario
+    }
+
+    render() {
+        const state = this.state
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label >
+                    Nome:
+                    <input type="text" name='name' ref={this.inputName} />
+                </label>
+                <input type="submit" />
+                {console.log(this.inputName)}
+            </form>
+
+        );
+
+    }
+}
